fb8cfdbd4dcbd5dd41c883f497e05cfcc16acc66
Harleen Kaur Sidhu <harleen.k.sidhu@gmail.com>
2018-04-04 07:58:46 +0800

    adding tests

--- app/src/test/java/com/example/asus/klasseandroid/PdfAdapterTest.java
+++ app/src/test/java/com/example/asus/klasseandroid/PdfAdapterTest.java
@@ -8,11 +8,13 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowToast;
 
 import feedback.PDF;
 import feedback.PdfAdapter;
 import feedback.WeekView;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 /**
@@ -21,23 +23,46 @@ import static org.junit.Assert.*;
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class PdfAdapterTest {
-    PdfAdapter pdfAdapter
+    PdfAdapter pdfAdapter;
 
-    @Test
-    public void testSubmitFeedback() throws Exception {
-
-        Button button = (Button) pdfAdapter.findViewById(R.id.pdffeedback);
-
-        button.performClick();
-        onView(withId(R.id.myDialogTextId)).check(matches(allOf(withText(myDialogText), isDisplayed()));
-
-
-
-//        EditText input = (EditText) pdfAdapter.findViewById(R.id.input);
-//        String txt=input.getText().toString();
-//        chatMessage = new ChatMessage(txt, "Harleen", "question", 11, id);
+//    @Test
+//    public void testDialogPopup() throws Exception {
 //
-//        assertEquals(chatMessage.getMessageType(),"question");
-    }
+//        Button button = (Button) pdfAdapter.findViewById(R.id.pdffeedback);
+//
+//        button.performClick();
+//        onView(withId(R.id.dialog_rl)).check(matches(allOf(withText(myDialogText), isDisplayed()));
+//
+//
+//
+////        EditText input = (EditText) pdfAdapter.findViewById(R.id.input);
+////        String txt=input.getText().toString();
+////        chatMessage = new ChatMessage(txt, "Harleen", "question", 11, id);
+////
+////        assertEquals(chatMessage.getMessageType(),"question");
+//    }
+//
+//    @Test
+//    public void testCancelButton() throws Exception{
+//        Button button = (Button) pdfAdapter.findViewById(R.id.cancel_button);
+//        onView(withId(android.R.id.button2)).perform(click());
+//
+//    }
+//    @Test
+//    public void testButtonClickShouldShowToast() throws Exception {
+//        PdfAdapter activity = Robolectric.buildActivity(PdfAdapter.class).create().get();
+//        Button view = (Button) activity.findViewById(R.id.dialog_negative_btn);
+//        assertNotNull(view);
+//        view.performClick();
+//        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("No button clicked") );
+//    }
+//    @Test
+//    public void testDialogSubmitButtonIfNoInput() throws Exception {
+//        Button submit = Button) pdfAdapter.findViewById(R.id.dialog_positive_btn);
+//        assertNotNull(submit);
+//        submit.performClick();
+//        String txt = "";
+//        assertEquals((String)ShadowToast.getTextOfLatestToast(),"Feedback empty!");
+//    }
 
 }
