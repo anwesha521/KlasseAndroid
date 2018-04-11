31ab377502eb8c5c333a56d63aa47323a926d815
roshnins <roshnins@hotmail.com>
2018-04-11 13:35:38 +0800

    test

--- app/src/test/java/com/example/asus/klasseandroid/StudentViewQuizTest.java
+++ app/src/test/java/com/example/asus/klasseandroid/StudentViewQuizTest.java
@@ -24,7 +24,7 @@ import org.robolectric.shadows.ShadowToast;
 import static android.content.Context.MODE_PRIVATE;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertNotEquals;
 import com.example.asus.klasseandroid.StudentViewQuiz.*;
 
 import java.util.ArrayList;
